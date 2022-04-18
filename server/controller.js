2e55c74fdda7c51451e91ed433741fae22e25069
Nicholas Anderson <anderson.nick@me.com>
2022-04-18 14:33:10 -0700

    updated db url

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const {CONNECTION_STRING} = process.env
+const {DATABASE_URL} = process.env
 
 // const { default: axios } = require("axios")
 const exp = require('constants')
@@ -8,7 +8,7 @@ const { builtinModules } = require('module')
 const Sequelize = require('sequelize')
 
 
-const sequelize = new Sequelize(CONNECTION_STRING, {
+const sequelize = new Sequelize(DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
         ssl:{
