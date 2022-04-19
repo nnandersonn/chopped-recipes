53508ed89e498e38f37c3dbb4f572624772da945
Nicholas Anderson <anderson.nick@me.com>
2022-04-19 08:41:36 -0700

    improved how recipe is passed to edit

--- public/main.js
+++ public/main.js
@@ -18,14 +18,12 @@ const instructionSection = document.getElementById('back-instructions')
 const editButtonSection = document.getElementById('edit-recipe-section')
 const submitRecipeSection = document.getElementById('submit-recipe-section')
 const deleteButtonSection = document.getElementById('delete-recipe-section')
-
+let recipeList = null
 
 
 const deleteRecipe = (recipe_id) =>{
-    console.log('my recipe id is', recipe_id)
     axios.delete(`api/recipe/${recipe_id}`)
         .then(()=>{
-            console.log('recipe deleted!')
             flipCard()
             fetchRecipes()
         })
@@ -33,16 +31,17 @@ const deleteRecipe = (recipe_id) =>{
     
 }
 
-const editRecipe = (recipe_id, recipe_title, ingredients, instructions ) => {
+const editRecipe = (recipe_id) => {
     flipCard360()
+    let selectedRecipe = getRecipeInfo(recipe_id)
     editButtonSection.innerHTML = ''
     deleteButtonSection.innerHTML = ''
     submitRecipeSection.innerHTML = '<button id="make-changes-button">Make Changes</button>'
     const makeChangesButton = document.getElementById('make-changes-button')
 
-    mainCardTitle.innerHTML = `<input type="text" id="edited-title" value="${recipe_title}" required>`
-    ingredientSection.innerHTML =`<textarea id="edited-ingredients">${ingredients}</textarea required>`
-    instructionSection.innerHTML =`<textarea id="edited-instructions">${instructions}</textarea required>`
+    mainCardTitle.innerHTML = `<input type="text" id="edited-title" value="${selectedRecipe.recipe_title}" required>`
+    ingredientSection.innerHTML =`<textarea id="edited-ingredients">${selectedRecipe.ingredients}</textarea required>`
+    instructionSection.innerHTML =`<textarea id="edited-instructions">${selectedRecipe.instructions}</textarea required>`
 
 
     makeChangesButton.addEventListener('click', ()=>{
@@ -57,10 +56,7 @@ const editRecipe = (recipe_id, recipe_title, ingredients, instructions ) => {
 const displayRecipe = (recipe) =>{
     flipCard()
     let {recipe_id, recipe_title, ingredients, instructions } = recipe
-    console.log(recipe_title, ingredients, instructions)
-    let strippedIngredients = ingredients.replace(/[\n\r]/g, '<br>')
-    let strippedInstructions = instructions.replace(/[\n\r]/g, '<br>')
-    editButtonSection.innerHTML = `<button onclick="editRecipe('${recipe_id}', '${recipe_title}', '${strippedIngredients}', '${strippedInstructions}')">Edit Recipe</button>`
+    editButtonSection.innerHTML = `<button onclick="editRecipe('${recipe_id}')">Edit Recipe</button>`
     deleteButtonSection.innerHTML = `<button onclick="deleteRecipe('${recipe_id}')">Delete Recipe</button>`
     
     spacedIngredients = ingredients.replace(/[\n\r]/g, '<br><br>')
@@ -72,8 +68,6 @@ const displayRecipe = (recipe) =>{
 }
 
 const createTitleButton = (arr) => {
-    // allRecipes = arr
-    console.log('running create title button', arr)
     for(let i = 0; i < arr.length; i++){
         const recipeTitleButton = document.createElement('button')
         recipeTitleButton.setAttribute("class", "recipe-title-button")
@@ -81,8 +75,6 @@ const createTitleButton = (arr) => {
         recipeTitleButton.addEventListener('click', () => displayRecipe(arr[i]))
         frontRecipeListSection.appendChild(recipeTitleButton)
     }
-    
-
 }
 
 const displayInputField = () => {
@@ -101,23 +93,18 @@ const displayInputField = () => {
         inputTitle = inputTitle.replace(/[']/g, `''`)
         inputIngredients = inputIngredients.replace(/[']/g, `''`)
         inputInstructions = inputInstructions.replace(/[']/g, `''`)
-        console.log(inputTitle, inputIngredients, inputInstructions)
         const newRecipe = {title: inputTitle, ingredients: inputIngredients, instructions: inputInstructions}
 
         axios.post('/api/addrecipe', newRecipe)
             .then(()=> {
-                console.log('axios .then ran in main.js')
                 fetchRecipes()
                 flipCard()
             })
-            .catch(err => console.log(err))
-            
+            .catch(err => console.log(err))      
     })
-
 }
 
 const submitChanges = (id, title, ingredients, instructions)=>{
-    console.log('id, title, ingredients, instructions', id, title, ingredients, instructions)
     
     axios.put(`/api/recipe/${id}`, {title, ingredients, instructions})
         .then(()=> {
@@ -130,18 +117,25 @@ const submitChanges = (id, title, ingredients, instructions)=>{
 
 
 const fetchRecipes = ()=>{
-    console.log('fetching all recipes')
-    
 
     axios.get('/api/getrecipes')
         .then(res => {
             frontRecipeListSection.innerHTML = ''
             createTitleButton(res.data)
+            recipeList = res.data
+            console.log('recipe list is -', recipeList[0].recipe_id)
         })
         .catch(err => {
             console.log(err)
         })
+}
 
+function getRecipeInfo(recipeID){
+    for(let i = 0; i < recipeList.length; i++){
+        if(recipeList[i].recipe_id == recipeID){
+            return recipeList[i]
+        }
+    }
 }
 
 window.onload = fetchRecipes()
@@ -155,12 +149,9 @@ function flipCard(){
     deleteButtonSection.innerHTML = ``
     submitRecipeSection.innerHTML = ``
     flipCardInner.classList.remove("flip360")
-    // flipCardFront.setAttribute("z-index", 0)
-    // flipCardBack.setAttribute("z-index", 1)
     console.log(flipCardFront)
     console.log(flipCardBack)
 }
 function flipCard360(){
     flipCardInner.classList.toggle("flip360")
-    // flipCardInner.classList.toggle("flip")
 }
